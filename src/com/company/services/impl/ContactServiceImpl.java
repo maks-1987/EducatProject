435c9d866bbd42c02a61bad593e5774350a09d1e
maks-1987 <max.moderation@gmail.com>
2018-04-05 20:11:18 +0300

    implementation of methods delContact

--- src/com/company/services/impl/ContactServiceImpl.java
+++ src/com/company/services/impl/ContactServiceImpl.java
@@ -7,6 +7,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public class ContactServiceImpl implements ContactService {
@@ -20,22 +21,51 @@ public class ContactServiceImpl implements ContactService {
             System.out.println(contact);
         }
     }
-    /*@Override
-    public void deleteContact(List<Contact> contactList, String name) {
+    @Override
+    public void deleteContact() throws IOException {
         Iterator<Contact> iterator = contactList.iterator();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Enter a name to delete...");
+        String name = br.readLine();
         while(iterator.hasNext()) {
-            if (iterator.next().getName() == name) {
+            if (iterator.next().getName().contains(name)) {
                 iterator.remove();
-                return;
+                System.out.println("  Contact deleted");
+            }/*else{
+                System.out.println("Retry...");
+            }*/
             }
         }
-    }*/
-    @Override
-    public void deleteContact() throws IOException, IndexOutOfBoundsException {
+    /*@Override
+    public void deleteContact() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        System.out.println(" Deleting a contact by index (0 - ...) N_ ");
-        /*String name = br.readLine();
-        int age = Integer.parseInt(br.readLine());
+        System.out.println("Enter a name to delete...");
+        String name = br.readLine();
+        Iterator iterator =
+        if (name.contains(name)) {
+            contactList.remove(this.contactList);
+        } else {
+            System.out.println("NOT");
+        }
+    }*/
+    /*@Override
+        public void deleteContact() throws IOException, IndexOutOfBoundsException {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            System.out.println(" Deleting a contact by index (0 - ...) N_ ");
+            int index = Integer.parseInt(br.readLine());
+            try {
+                if (index >= 0) {
+                    contactList.remove(index);
+                    System.out.println("Contact deleted");
+                } else { // если число < 0 выведем это:
+                    System.out.println(" Incorrect index. Repeat! Delete Contact");
+                } // если ввести число > чем индексов массива выведем это:
+            } catch (IndexOutOfBoundsException e) {
+                System.out.println(" Incorrect index. Repeat! Delete Contact");
+            }
+        }*/
+    //String name = br.readLine();
+        /*int age = Integer.parseInt(br.readLine());
         String phoneNumber = br.readLine();
         Contact contact = new Contact(name, age, phoneNumber);
         if (contact.equals(Contact))
@@ -43,18 +73,10 @@ public class ContactServiceImpl implements ContactService {
         else
             System.out.println("NOT");
         */
-        int index = Integer.parseInt(br.readLine());
-        try {
-            if (index >= 0) {
-                contactList.remove(index);
-                System.out.println("Contact deleted");
-            } else { // если число < 0 выведем это:
-                System.out.println(" Incorrect index. Repeat! Delete Contact");
-            } // если ввести число > чем индексов массива выведем это:
-        } catch (IndexOutOfBoundsException e) {
-            System.out.println(" Incorrect index. Repeat! Delete Contact");
-        }
-    }
+        /*Iterator<Contact> iterator = contactList.iterator();
+        iterator.next().getName(br.readLine());*/
+    //contactList.remove(this.contactList);
+    //contactList.remove(Contact);
     @Override
     public void showContact() {
         if (contactList.isEmpty()) { // проверка, пустой ли список
